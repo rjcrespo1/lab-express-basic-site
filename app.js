a97a06840507976f2dbbb65255e9899dbb79d958
Ryan Crespo <crespoffi@gmail.com>
2021-12-04 16:54:57 -0500

    done

---
+++ app.js
@@ -0,0 +1,14 @@
+const express = require('express');
+const app = express();
+
+
+
+app.get('/', (req, res) => res.sendFile(__dirname + '/views/index.html'));
+app.get('/about', (req, res) => res.sendFile(__dirname + '/views/about.html'));
+app.get('/works', (req, res) => res.sendFile(__dirname + '/views/works.html'));
+app.get('/gallery', (req, res) => res.sendFile(__dirname + '/views/gallery.html'));
+
+
+
+
+app.listen(3000, () => console.log('Running'));
